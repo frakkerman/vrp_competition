acc50dc3ccfff44b843aab07f83a351dbd3099b9
Akkerman <f.r.akkerman@utwente.nl>
2024-02-02 16:04:08 +0100

    big update

--- GroupTemplate/main.py
+++ GroupTemplate/main.py
@@ -21,19 +21,22 @@ def main(instance):
     # Example operation on the instance
     print(instance.keys())
 
+    # Example using imported module Helper
+    test()
+
     # For this example, we directly return a solution read from a file
-    # students should implement logic to generate the solution, so it is not enough to just provide the solution
+    # students should implement logic to generate the solution, so it is not enough to just provide the solution file
     # NOTE: please remove all loading, writing, and print statements from your submission code, i.e., comment the
     # line below, since it will yield an error on the submission platform since the provided path is not recognized
     solution = vrplib.read_solution("../Instances/Test/Sol/X-n101-k25.sol")
 
     # see vrplib documentation for some reading and writing tools
 
-
     return solution #solution in vrplib format
 
-# NOTE: comment out all the below lines before submission, this code is only useful for locally running your,
+# NOTE: comment out all the below lines before submission, this code is only useful for locally running your code,
 # but will yield errors when submitting
+
 
 if __name__ == "__main__":
     # Read VRPLIB formatted instances, here you can read any instance you want locally
--- Groups/Group1/main.py
+++ Groups/Group1/main.py
@@ -2,9 +2,10 @@ import vrplib
 import numpy as np
 import scipy
 import pandas as pd
+from helper import test
 
 # These are the libraries we provide by default. If you want to use different Python libraries:
-# please let us know so that we can check if we can add those to the competition environment.
+# please let us know so that we can look if these can be added to the competition environment.
 # Note: importing a library that is not listed above will result in an error when submitting your code
 
 
@@ -17,20 +18,27 @@ def main(instance):
         solution (vrplib): solution in vrplib format.
     """
 
-    # get instance keys
+    # Example operation on the instance
     print(instance.keys())
 
-    # For this test, we provide the solution. Note that we expect you to provide us with the code that obtains the
-    # solution, i.e., just providing solution files is not enough. The solution returned only requires the routes and
-    # sequence of visits, e.g., costs of routes do not need to be included as we calculate them ourselves
-    solution = vrplib.read_solution("Instances/Test/Sol/X-n101-k25.sol")
+    # Example using imported module Helper
+    test()
+
+    # For this example, we directly return a solution read from a file
+    # students should implement logic to generate the solution, so it is not enough to just provide the solution file
+    # NOTE: please remove all loading, writing, and print statements from your submission code, i.e., comment the
+    # line below, since it will yield an error on the submission platform since the provided path is not recognized
+    solution = vrplib.read_solution("../Instances/Test/Sol/X-n101-k25.sol")
 
     # see vrplib documentation for some reading and writing tools
 
-    return solution
+    return solution #solution in vrplib format
+
+# NOTE: comment out all the below lines before submission, this code is only useful for locally running your code,
+# but will yield errors when submitting
 
 
 if __name__ == "__main__":
-    #Read VRPLIB formatted instances, here you can read any instance you want
-    instance = vrplib.read_instance("Instances/Test/X-n101-k25.vrp")
+    # Read VRPLIB formatted instances, here you can read any instance you want locally
+    instance = vrplib.read_instance("../Instances/Test/X-n101-k25.vrp")
     sol = main(instance)
