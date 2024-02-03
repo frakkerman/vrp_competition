b0f87dc79df0802facea2ed265dd4574aa2a014e
Akkerman <f.r.akkerman@utwente.nl>
2024-02-03 12:22:45 +0100

    added random solution

--- GroupTemplate/main.py
+++ GroupTemplate/main.py
@@ -4,6 +4,7 @@ import scipy
 import pandas as pd
 import os
 import sys
+import random
 # Get the directory of the currently executing script (main.py)
 current_script_directory = os.path.dirname(os.path.realpath(__file__))
 # Construct the path to helper.py relative to the current script
@@ -30,20 +31,36 @@ def main(instance):
     # Example operation on the instance
     print(instance.keys())
 
-    # Example using imported module Helper
-    helper.test()
+    # obtain the coordinates of customers. Note that index=0 is the depot
+    # in your solution, you do not need to include the start and finish at the depot, as it is
+    # implicitly assumed that all vehicles start and end at the depot
+    coords = helper.parse_instance(instance)
 
-    # For this example, we directly return a solution read from a file
-    # students should implement logic to generate the solution, so it is not enough to just provide the solution file
-    # NOTE: please remove all loading, writing, and print statements from your submission code, i.e., remove the
-    # line below, since it will yield an error on the submission platform since the provided path is not recognized
-    solution = vrplib.read_solution("../Instances/Test/Sol/X-n101-k25.sol")
-    # Example operation on the solution
-    print(solution.keys())
+    # remove depot from list
+    coords = coords[1:, :]
 
-    # see vrplib documentation for some reading and writing tools
+    # For this example, we implement a function that returns a random route. It is your task to come up
+    # with a better approach to reduce the total distance covered
+    n_customers = coords.shape[0]  # Number of customers
 
-    return solution # you need to return a solution in vrplib format
+    # Generate a list of all customer indices
+    customer_indices = list(range(1, n_customers + 1))  # Starting from 1 to n_customers
+
+    # Shuffle the customer indices to randomize the route
+    random.shuffle(customer_indices)
+
+    # Generate the random routes, you should come upt with something better...
+    random_routes = helper.generate_random_routes(customer_indices)
+
+    # Create the solution dictionary
+    solution = {'routes': random_routes}
+    print('Random sequence solution')
+    print(solution)
+
+    # students should implement logic to generate the solution, it is not enough to just provide the solution sequence
+    # NOTE: please remove all loading, writing, and print statements from your submission code
+
+    return solution  # you need to return a solution in this exact (vrplib) format
 
 # NOTE: comment out all the below lines before submission, this code is only useful for locally running your code,
 # but will yield errors when submitting
